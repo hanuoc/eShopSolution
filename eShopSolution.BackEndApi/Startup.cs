5d948eb99c02d134733d7cf594b4e1f6b1b4862b
hanuoc <hvndoatmenh@gmail.com>
2020-06-24 21:50:41 +0700

    add validation with fluent api

--- eShopSolution.BackEndApi/Startup.cs
+++ eShopSolution.BackEndApi/Startup.cs
@@ -8,6 +8,9 @@ using eShopSolution.Application.System.Users;
 using eShopSolution.Data.EF;
 using eShopSolution.Data.Entities;
 using eShopSolution.Utilities.Constants;
+using eShopSolution.ViewModels.System.Users.FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -16,6 +19,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace eShopSolution.BackEndApi
@@ -34,6 +38,7 @@ namespace eShopSolution.BackEndApi
 		{
 			services.AddDbContext<EShopDbContext>(options =>
 			 options.UseSqlServer(Configuration.GetConnectionString(SystemConstants.MainConnectionString)));
+			services.AddCors();
 			services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<EShopDbContext>().AddDefaultTokenProviders();
 			// Declare DI
 			services.AddTransient<IStorageService, FileStorageService>();
@@ -46,8 +51,62 @@ namespace eShopSolution.BackEndApi
 			services.AddSwaggerGen(c =>
 			{
 				c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger Api EShopSolution", Version = "v1" });
+				c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+				{
+					Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+					Name = "Authorization",
+					In = ParameterLocation.Header,
+					Type = SecuritySchemeType.ApiKey,
+					Scheme = "Bearer"
+				});
+
+				c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+				  {
+					{
+					  new OpenApiSecurityScheme
+					  {
+						Reference = new OpenApiReference
+						  {
+							Type = ReferenceType.SecurityScheme,
+							Id = "Bearer"
+						  },
+						  Scheme = "oauth2",
+						  Name = "Bearer",
+						  In = ParameterLocation.Header,
+						},
+						new List<string>()
+					  }
+					});
 			});
-			services.AddControllersWithViews();
+			services.AddControllers().AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<LoginRequestValidator>());
+			string issuer = Configuration.GetValue<string>("Tokens:Issuer");
+			string signingKey = Configuration.GetValue<string>("Tokens:Key");
+			byte[] signingKeyBytes = System.Text.Encoding.UTF8.GetBytes(signingKey);
+
+			services.AddAuthentication(opt =>
+			{
+				opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+			})
+			.AddJwtBearer(options =>
+			{
+				options.RequireHttpsMetadata = false;
+				options.SaveToken = true;
+				options.TokenValidationParameters = new TokenValidationParameters()
+				{
+					ValidateIssuer = true,
+					ValidIssuer = issuer,
+					ValidateAudience = true,
+					ValidAudience = issuer,
+					ValidateLifetime = true,
+					ValidateIssuerSigningKey = true,
+					ClockSkew = System.TimeSpan.Zero,
+					IssuerSigningKey = new SymmetricSecurityKey(signingKeyBytes)
+				};
+			});
+
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,7 +124,8 @@ namespace eShopSolution.BackEndApi
 			}
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
-
+			app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+			app.UseAuthentication();
 			app.UseRouting();
 
 			app.UseAuthorization();
